eb40b41d901d79764e6d5d34ced2ec6d526f17cf
NyAndriaX <tsilavinaandriamahafaly01@gmail.com>
2024-11-14 16:27:05 +0300

    add list site 1

--- src/app/[locale]/components/SpacesDialog.tsx
+++ src/app/[locale]/components/SpacesDialog.tsx
@@ -20,7 +20,7 @@ export const SpacesDialog: React.FC<SpacesDialogProps> = ({
                 onClick={onHide}
             />
             <div
-                className="fixed bg-white z-50 h-full rounded-md max-w-80"
+                className="fixed bg-white w-72 z-50 h-full rounded-md"
                 style={{ left: `${positionLeft}px` }}
             >
                 <div className="flex flex-col gap-8 items-start py-8 px-6 text-gray-900">
@@ -28,19 +28,29 @@ export const SpacesDialog: React.FC<SpacesDialogProps> = ({
                         <h2 className="text-lg font-semibold">Espaces</h2>
                         <div className="flex flex-col gap-4 bg-gray-50 p-4 rounded-md w-full shadow-sm">
                             <p className="text-sm font-semibold text-gray-500">
-                                Participez à la communauté
+                                Intégrez votre site
                             </p>
                             <Link
                                 className="flex items-center text-sm font-bold gap-2 w-fit border-b border-gray-900 pb-1"
                                 href="/pages/member/topic"
                                 onClick={onHide}
                             >
-                                <span>Nouveau sujet</span>
+                                <span>Ajouter un site</span>
                                 <span
                                     className="pi pi-arrow-right text-gray-500"
                                     style={{ fontSize: '0.8rem' }}
                                 />
                             </Link>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-4 items-start w-full">
+                        <div className="flex items-center gap-4 text-sm text-gray-900 font-semibold cursor-pointer bg-white hover:bg-gray-100 w-full p-4 rounded-md">
+                            <span
+                                className="pi pi-globe"
+                                style={{ fontSize: '1rem' }}
+                            />
+                            <span>Nom du site 1</span>
                         </div>
                     </div>
                 </div>
